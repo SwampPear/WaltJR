e910cd56d868d3afab6230f3be5efec043235c66
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-24 22:49:14 -0500

    rebuilt command to compile contract

--- scripts/compile.py
+++ scripts/compile.py
@@ -1,1 +1,14 @@
 # TODO: remake compilation script
+import os
+import sys
+
+
+def compile():
+    for file_name in os.listdir('contracts'):
+        output_dir = f"artifacts/{file_name.split('.')[0]}"
+
+        command = f'solc -o {output_dir} --bin --ast-compact-json --asm contracts/{file_name}'
+
+        os.system(command)
+
+compile()
