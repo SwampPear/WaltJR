9ff488cfc41af54f4019c6ba7bafb43f5a6c7c95
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-26 10:01:12 -0500

    completed base flash loan

---
+++ contract/contracts/FlashLoan.sol
@@ -0,0 +1,52 @@
+// Specifies the version of Solidity, using semantic versioning.
+// Learn more: https://solidity.readthedocs.io/en/v0.5.10/layout-of-source-files.html#pragma
+pragma solidity >=0.6.6;
+
+import "./contracts/aave/FlashLoanReceiverBase.sol";
+import "./contracts/aave/ILendingPoolAddressesProvider.sol";
+import "./contracts/aave/ILendingPool.sol";
+
+contract FlashLoan is FlashLoanReceiverBase {
+
+    constructor(address _addressProvider) FlashLoanReceiverBase(_addressProvider) public {}
+
+    /**
+        This function is called after your contract has received the flash loaned amount
+     */
+    function executeOperation(
+        address _reserve,
+        uint256 _amount,
+        uint256 _fee,
+        bytes calldata _params
+    )
+        external
+        override
+    {
+        require(_amount <= getBalanceInternal(address(this), _reserve), "Invalid balance, was the flashLoan successful?");
+
+        //
+        // Your logic goes here.
+        // !! Ensure that *this contract* has enough of `_reserve` funds to payback the `_fee` !!
+        //
+
+        uint totalDebt = _amount.add(_fee);
+        transferFundsBackToPoolInternal(_reserve, totalDebt);
+    }
+
+    function flashloan() public onlyOwner {
+        /**
+        * Flash Loan of 1000 DAI
+        */
+        address receiver = address(this); // Can also be a separate contract
+        address asset = "0x6b175474e89094c44da98b954eedeac495271d0f"; // Dai
+        uint256 amount = 1000 * 1e18;
+        
+        // If no params are needed, use an empty params:
+        bytes memory params = "";
+        // Else encode the params like below (bytes encoded param of type `address` and `uint`)
+        // bytes memory params = abi.encode(address(this), 1234);
+        
+        ILendingPool lendingPool = ILendingPool(addressesProvider.getLendingPool());
+        lendingPool.flashLoan(address(this), asset, amount, params);
+    }
+}
