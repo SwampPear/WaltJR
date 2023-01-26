78ae24c3d396f3d0a7f75b7edc66ee7df8a6a3a7
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-25 22:39:04 -0500

    contract deployment pipeline setup

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -2,3 +2,20 @@
 module.exports = {
   solidity: "0.8.17",
 };
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { INFURA_URL_ENDPOINT_SEPOLIA, PRIVATE_KEY } = process.env;
+
+module.exports = {
+   solidity: "0.8.17",
+   defaultNetwork: "sepolia",
+   networks: {
+      hardhat: {},
+      sepolia: {
+         url: INFURA_URL_ENDPOINT_SEPOLIA,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
