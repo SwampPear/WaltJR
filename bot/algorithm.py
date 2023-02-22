0ebfd1af2a68b87531c0addfc8fb89b326a75d41
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-02-22 00:25:42 -0500

    asd

--- bot/algorithm.py
+++ bot/algorithm.py
@@ -1,58 +1,151 @@
-class Node:
+class Vertex:
     """
-    This class represents a Node object to be used with the
+    This class represents a Vertex object to be used with the
     Graph class.
 
     Attributes
-        data_class (str): the class of data that a node could be
-        data_enum (str): data individualizing each node
+        data_class (str): the class of data that a vertex could be
+        data_enum (str): data individualizing each vertex
     """
     
-    def __init__(self, data_class, data_enum):
+    def __init__(self, data_class, data_enum, weights=[]):
         """
-        Initializes the Node class.
+        Initializes the Vertex class.
 
         Parameters
-            data_class (str): the class of data that a node could be
-            data_enum (str): data individualizing each node
+            weights (Tuple(float, Vertex)): weights defining edges
+            data_class (str): the class of data that a vertex could be
+            data_enum (str): data individualizing each vertex
         """
-        self.weights = []
+        self.weights = weights
         self.data_class = data_class
         self.data_enum = data_enum
 
-    def emplace(self, weight, node):
+    def emplace(self, weight, vertex):
         """
-        Adds a new node to the weight graph.
+        Adds a new edge to the weight graph.
 
         Parameters
             weight (float): the weight to set for the edge
-            node (Node): the next node in succession
+            vertex (Vertex): the vertex directed to
         """
         
-        self.weights.append((weight, node))
+        self.weights.append((weight, vertex))
 
         
 class Graph:
     """
-    This class repsents a graph object where two nodes exist
-    of each type and each direction of edge between each node is
-    present. Each node of the same type should have a weight of
-    1. The graph is directed.
+    This class repsents a directed graph object where two vertices
+    exist of each type and each direction of edge between each
+    vertex is present. Each vertex of the same type should have a
+    weight of 1.
+
+    Data should be of format:
+
+    data = [
+        {
+            'exchange': 'curve',
+            'currencies': [
+                'dai',
+                'usdc'
+            ],
+            'pairs': [
+                {
+                    'a': 'dai',
+                    'b': 'usdc',
+                    'rate': 1
+                }
+            ]
+        }
+    ]
+
+    Attributes
+        vertices (Vertex[]): vertices in this graph
     """
     
+
     def __init__(self, data):
         """
         Initializes the Graph class.
         """
+
+        self.vertices = []
+
+
+    def add_vertex(self, data_class, data_enum):
+        """
+        Adds a vertex to vertices.
+        """
+
+        _vertex = Vertex(data_class, data_enum)
         
-        self.head = None
+        self.vertices.append(_vertex)
 
 
-    def initialize(self, data):
+    def get_vertex(self, data_class, data_enum):
         """
-        Initializes the data representation of the graph with
-        data of a digestible data structure.
+        Gets a vertex from this Graph object.
         """
+
+        for _vertex in self.vertices:
+            if _vertex.data_class == data_class and _vertex.data_enum == data_enum:
+                return _vertex
+
+        return None
+
+
+    def remove_vertex(self, data_class, data_enum):
+        """
+        Removes a vertex from this Graph object.
+        """
+
+        _vertex = self.get_vertex(data_class, data_enum)
+
+        self.vertices.remove(_vertex)
+
+        
+    def _initialize_vertices(self, data):
+        """
+        Initializes the vertices for this graph.
+        """
+
+        for _exchange in data:
+            for _currency in _exchange['currencies']:
+                self.graph.add_vertex(_currency, _exchange['exchange'])
+
+
+    def _initialize_edges(self, data):
+        """
+        Initializes the edges for this graph based on the data pattern.
+        """
+
+        pass
+    
+        
+    def _initialize_graph(self, data):
+        """
+        Initializes the graph with data.
+        """
+
+        self._initialize_vertices(data)
+        self._initialize_edges(data)
+
+
+data = [
+    {
+        'exchange': 'curve',
+        'currencies': [
+            'dai',
+            'usdc'
+        ],
+        'pairs': [
+            {
+                'a': 'dai',
+                'b': 'usdc'
+            }
+        ]
+    }
+]
 
         
 
